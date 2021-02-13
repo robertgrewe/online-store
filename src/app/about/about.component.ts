9af87a1505c941fc89c8928c7156b07434e673f1
robertgrewe <48450774+robertgrewe@users.noreply.github.com>
2021-02-13 23:43:54 +0100

    Add social login.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { SocialAuthService } from "angularx-social-login";
+import { SocialUser } from "angularx-social-login";
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+  VKLoginProvider,
+  MicrosoftLoginProvider
+} from "angularx-social-login";
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -7,9 +17,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  user: SocialUser;
+  GoogleLoginProvider = GoogleLoginProvider;
+
+  constructor(private authService: SocialAuthService) { }
 
   ngOnInit() {
+    this.authService.authState.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithAmazon(): void {
+    this.authService.signIn(AmazonLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithVK(): void {
+    this.authService.signIn(VKLoginProvider.PROVIDER_ID);
+  }
+
+  signInWithMicrosoft(): void {
+    this.authService.signIn(MicrosoftLoginProvider.PROVIDER_ID);
+  }
+
+  signOut(): void {
+    this.authService.signOut();
+  }
+
+  refreshGoogleToken(): void {
+    this.authService.refreshAuthToken(GoogleLoginProvider.PROVIDER_ID);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,14 @@ import {CartService} from "./services/cart.service";
 import {CartPopupComponent} from "./pages/cart/cart-popup/cart-popup.component";
 import {ProductService} from "./services/products.service";
 import { HomeComponent } from './home/home.component';
+import { SocialLoginModule, SocialAuthServiceConfig } from "angularx-social-login";
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider,
+  AmazonLoginProvider,
+  VKLoginProvider,
+  MicrosoftLoginProvider
+} from "angularx-social-login";
 
 @NgModule({
     declarations: [
@@ -26,9 +34,45 @@ import { HomeComponent } from './home/home.component';
         BrowserModule,
         FormsModule,
         HttpModule,
+        SocialLoginModule,
         RouterModule.forRoot(appRoutes)
     ],
-    providers: [CartService,ProductService],
+    providers: [CartService,ProductService,
+      {
+        provide: 'SocialAuthServiceConfig',
+        useValue: {
+          autoLogin: true,
+          providers: [
+            {
+              id: GoogleLoginProvider.PROVIDER_ID,
+              provider: new GoogleLoginProvider(
+                '624796833023-clhjgupm0pu6vgga7k5i5bsfp6qp6egh.apps.googleusercontent.com'
+              ),
+            },
+            {
+              id: FacebookLoginProvider.PROVIDER_ID,
+              provider: new FacebookLoginProvider('561602290896109'),
+            },
+            {
+              id: AmazonLoginProvider.PROVIDER_ID,
+              provider: new AmazonLoginProvider(
+                'amzn1.application-oa2-client.f074ae67c0a146b6902cc0c4a3297935'
+              ),
+            },
+            {
+              id: VKLoginProvider.PROVIDER_ID,
+              provider: new VKLoginProvider(
+                '7624815'
+              ),
+            },
+            {
+              id: MicrosoftLoginProvider.PROVIDER_ID,
+              provider: new MicrosoftLoginProvider('0611ccc3-9521-45b6-b432-039852002705'),
+            }
+          ],
+        } as SocialAuthServiceConfig,
+      }
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
